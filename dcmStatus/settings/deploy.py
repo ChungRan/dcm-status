3591e8da5451a8eee75fdedc5183bf0995ad2852
ChungRan <riplac0314@gmail.com>
2021-03-28 16:50:04 +0900

    test groom

--- dcmStatus/settings/deploy.py
+++ dcmStatus/settings/deploy.py
@@ -2,4 +2,8 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
+
+CRONJOBS = [
+    ('* 1 * * *', 'dcmStatus.cron.crawling_everyday', '>> /tmp/log/ggbc_cron.log'),
+]
